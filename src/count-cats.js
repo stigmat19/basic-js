8969e9cf3962ea5e2ae85aa2feb30ca10b7bd3bb
Yauheni1988 <stigmat19@yandex.ru>
2020-03-22 20:57:49 +0300

    first commit

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,17 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+
+  console.log('-----matrix', matrix);
+
+  let count = 0;
+
+  for(let i = 0; i < matrix.length; i++){
+    for (let k = 0; k < matrix[i].length; k++){
+      if(matrix[i][k] === '^^'){
+        count++;
+      }
+    }
+  }
+
+  return count;
+
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,28 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+
+    let res = '';
+
+    let clearMembers = [];
+
+    if(Array.isArray(members)){
+        for (let i = 0; i < members.length; i++){
+            if(typeof members[i] === 'string'){
+                clearMembers.push(members[i].trim());
+            }
+            else{
+                continue;
+            }
+        }
+
+        for(let i = 0; i < clearMembers.length; i++) {
+            res += clearMembers[i][0].toUpperCase()
+        }
+
+        return res.split('').sort().join('');
+    }
+    else{
+        return false;
+    }
+
+
 };
